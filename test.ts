3355b2cf56163dcd3d0bbeee1839c54ae7ce0ed4
gbantique <gbantique@gmail.com>
2023-10-17 09:05:30 +0800

    moved main.ts to test.ts for extension purpose

--- test.ts
+++ test.ts
@@ -1,1 +1,17 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+Brickcell.onPress(function () {
+    count = 0
+    serial.writeLine("" + (count))
+})
+Brickcell.onRotate(RotationDirection.Right, function () {
+    count += 1
+    serial.writeLine("" + (count))
+})
+Brickcell.onRotate(RotationDirection.Left, function () {
+    count += -1
+    serial.writeLine("" + (count))
+})
+let count = 0
+Brickcell.initRotary(DigitalPin.P0, DigitalPin.P1, DigitalPin.P2)
+serial.setBaudRate(BaudRate.BaudRate115200)
+count = 0
+serial.writeLine("" + (count))
